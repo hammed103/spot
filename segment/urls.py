9691e90954e699c3fd44b9e93a68d0b24355fa33
hammed103 <hammedfree@gmail.com>
2023-09-25 02:19:40 +0100

    mate

--- segment/urls.py
+++ segment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 
-from .new_views import start
+from .new_views import start,segment
 urlpatterns = [
     path("api/start", start.as_view()),
+     path("api/segment", start.as_view()),
 ]
