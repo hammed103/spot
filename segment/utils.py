8b976d046f6be7e05a4b2a69df4b01b8b89a723b
hammed103 <hammedfree@gmail.com>
2023-10-10 14:29:34 +0100

    mate

--- segment/utils.py
+++ segment/utils.py
@@ -94,7 +94,7 @@ def login(driver):
         username_input.send_keys("x@1111.io")
         sleep(1)
         username_input = driver.find_element(By.ID, "login-password")
-        username_input.send_keys("Speedbumps123@@_121!3")
+        username_input.send_keys("Speedbumps123@@_121!35")
         sleep(1)
         driver.find_element(By.ID, "login-button").click()
         print("password entered ...")
