e33d23d12beaa36d03278744a6649f0a6422071c
hammed103 <hammedfree@gmail.com>
2023-10-22 18:14:56 +0100

    mate

--- segment/new_views.py
+++ segment/new_views.py
@@ -621,7 +621,7 @@ class source(APIView):
             'spotify-app-version': '1.0.0.b149c28',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60',
         }
-        for id, namex in art[:]:
+        for id, namex in art[:3]:
               # cd = ""
             params = {
                 'time-filter': '28day',
