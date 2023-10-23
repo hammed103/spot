45d87159ecc3a7e5d4694e45e372369b96e5238f
hammed103 <hammedfree@gmail.com>
2023-10-23 13:13:08 +0100

    mate

--- segment/utils.py
+++ segment/utils.py
@@ -41,7 +41,7 @@ options = {
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run Chrome in headless mode
 # chrome_profile_path = "/Default"
-chrome_options.add_argument("--user-data-dir=Default")
+chrome_options.add_argument("--user-data-dir=Default2")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
