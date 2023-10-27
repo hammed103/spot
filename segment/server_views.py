34f6896e7fc69113c5bb5bfae0badb3676a28aeb
hammed103 <hammedfree@gmail.com>
2023-10-27 12:31:26 +0100

    runner

--- segment/server_views.py
+++ segment/server_views.py
@@ -15,8 +15,6 @@ class start(APIView):
     def get(req):
         from datetime import date, timedelta
 
-        # Create a new instance of ChromeDriver
-    # Create a new instance of ChromeDriver
         try:
 
             driver.refresh()
@@ -24,7 +22,8 @@ class start(APIView):
             driver = wirewebdriver.Chrome(service=service, options=chrome_options,seleniumwire_options=options)
 
             driver.get('https://artists.spotify.com/c/artist/0aUMVkR8QV0LSdv9VZOATn/home')
-        sleep(10)
+
+        sleep(20)
 
         for request in driver.requests:
             if request.headers:
@@ -32,6 +31,22 @@ class start(APIView):
                     auth_header = request.headers["Authorization"]
                     if auth_header != "":
                         break
+        from bs4 import BeautifulSoup
+        # Get the page source
+        sleep(5)
+        html_source = driver.page_source
+
+        # Use BeautifulSoup to parse the page source
+        soup = BeautifulSoup(html_source, 'html.parser')
+
+        # Find all h1 tags
+        h1_tags = soup.find_all('h1')
+        pa_tags = soup.find_all("span")
+        # Print the content of each h1 tag
+        for tag in h1_tags:
+            print(tag.text)
+        for tg in pa_tags:
+            print(tg.text)
 
         return Response(
             {
